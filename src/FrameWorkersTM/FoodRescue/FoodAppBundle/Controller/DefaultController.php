de1751a40d81f93338af4d281547cd85b7974a87
povlas <pmankus@gmail.com>
2014-03-25 19:00:58 +0200

    master init

---
+++ src/FrameWorkersTM/FoodRescue/FoodAppBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace FrameWorkersTM\FoodRescue\FoodAppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('FrameWorkersTMFoodRescueFoodAppBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/FrameWorkersTM/FoodRescue/FoodAppBundle/FrameWorkersTMFoodRescueFoodAppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FrameWorkersTM\FoodRescue\FoodAppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FrameWorkersTMFoodRescueFoodAppBundle extends Bundle
+{
+}
