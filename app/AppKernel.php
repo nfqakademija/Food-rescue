d7b808bc7dd2386caaf62abd34f4b3a2d9ca96ed
darbukas <darbukas@ubuntu.(none)>
2014-04-05 13:12:33 +0300

    testtuojam

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,12 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FrameWorkersTM\FoodRescue\FoodAppBundle\FrameWorkersTMFoodRescueFoodAppBundle(),
+
+            new FOS\UserBundle\FOSUserBundle(),
+            #Rolando demo
+            new Testukas\Bundle\TestasBundle\TestukasTestasBundle(),
+            #Rolando demo
+            new Acme\UserBundle\AcmeUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
