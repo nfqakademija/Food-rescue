381ebffdf068420349cea5c49b5c8c8f995efe5d
Povilas Mankus <pmankus@gmail.com>
2014-04-15 20:33:14 +0300

    - baigta produktų vaizdavimo forma

--- src/FrameWorkersTM/FoodRescue/FoodAppBundle/Controller/MyProductsController.php
+++ src/FrameWorkersTM/FoodRescue/FoodAppBundle/Controller/MyProductsController.php
@@ -7,8 +7,42 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MyProductsController extends Controller
 {
+    private function formRow($name, $quantity, $date, $units) {
+        $string = "
+            <div class=\"form-group row\"/>
+                <div class='col-sm-5 col-md-5 col-lg-5'>
+                    <input class=\"form-control\" type=\"text\" value=\"$name\" />
+                </div>
+                <div class='col-sm-2 col-md-2 col-lg-2'>
+                    <input class=\"form-control\" type=\"num\" value=\"$quantity\"/>
+                </div>
+                <div class='col-sm-1 col-md-2 col-lg-2'>
+                    <select class=\"form-control\" form=\"produktai\">
+                        <option value=\"g\">g</option>
+                        <option value=\"l\" selected='selected' >l</option>
+                        <option value=\"vnt\">vnt.</option>
+                    </select>
+                </div>
+                <div class='col-sm-2 col-md-2 col-lg-2'>
+                    <input class=\"form-control\" type='date' value='$date'>
+                </div>
+                <div class='col-sm-1 col-md-1 col-lg-1'>
+                    <button type='button' class='form-control'>
+                        <span class='glyphicon glyphicon-trash'></span>
+                    </button>
+                </div>
+            </div>
+        ";
+        return $string;
+    }
     public function indexAction()
     {
-        return $this->render('FrameWorkersTMFoodRescueFoodAppBundle:MyProducts:index.html.twig');
+
+        $array['forma'] = $this->formRow('Kiaušiniai', 2, '2014-04-27', 'vnt');
+        $array['forma'] .= $this->formRow('Pienas', 0.5, '2014-04-28', 'l');
+        $array['forma'] .= $this->formRow('Rūgpienis', 1, '2014-04-28', 'l');
+        $array['forma'] .= $this->formRow('Kiauliena', 800, '2014-04-28', 'g');
+
+        return $this->render('FrameWorkersTMFoodRescueFoodAppBundle:MyProducts:index.html.twig', $array);
     }
 }
