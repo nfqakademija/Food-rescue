53335e0291b414dddb99417dfd6ac3e056217c87
Povilas Mankus <pmankus@gmail.com>
2014-04-15 17:12:27 +0300

    -settings window

--- src/FrameWorkersTM/FoodRescue/FoodAppBundle/Controller/SettingsController.php
+++ src/FrameWorkersTM/FoodRescue/FoodAppBundle/Controller/SettingsController.php
@@ -12,6 +12,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class SettingsController extends Controller {
     public function indexAction()
     {
-        return $this->render('FrameWorkersTMFoodRescueFoodAppBundle:Settings:index.html.twig');
+        $form = $this->createFormBuilder()
+            ->add('email', 'email', array(
+                'label' => 'El. paštas'
+            ))
+            ->add('notificationTime', 'integer', array(
+                'label' => 'Priminti apie besibaigiančius produktus likus dienų: '
+            ))
+            ->add('sortRecipies', 'choice', array(
+                'label' => 'Receptus rūšiuoti pagal',
+                'choices' => array(
+                    'endDate' => 'greičiausiai pasibaigsiančius produktus',
+                    'favourites' => 'pomėgius'
+                )
+            ))
+            ->getForm();
+
+        return $this->render('FrameWorkersTMFoodRescueFoodAppBundle:Settings:index.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 } 
