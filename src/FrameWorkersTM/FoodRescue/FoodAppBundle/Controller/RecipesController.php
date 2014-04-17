932a833675706fffbdf820a2d37734a4e3e2159e
Karolis <karolis@webivision.lt>
2014-04-17 03:44:39 +0300

    - inner recipe page
    - Question about routing ( http://vagrant.dev/app_dev.php/recipes/view/omleto-receptas | http://vagrant.dev/app_dev.php/recipes/ - doesn`t work - WHY? )

--- src/FrameWorkersTM/FoodRescue/FoodAppBundle/Controller/RecipesController.php
+++ src/FrameWorkersTM/FoodRescue/FoodAppBundle/Controller/RecipesController.php
@@ -9,10 +9,26 @@ use Symfony\Component\HttpFoundation\Session\Session;
 
 class RecipesController extends Controller
 {
+    //TODO: Pasiklaust dėl login kūrimo
+    public function __construct(){
+        
+    }
+    
     public function indexAction(Request $request)
-    {
+    {   
         $session = $request->getSession();
-        $array['logged']= $session->get('logged');
+        $array['logged'] = $session->get('logged');
         return $this->render('FrameWorkersTMFoodRescueFoodAppBundle:Recipes:index.html.twig', $array);
     }
+    
+    public function RecipieViewAction($slug){
+        $request = Request::createFromGlobals();
+                
+        
+        
+        return $this->render('FrameWorkersTMFoodRescueFoodAppBundle:Recipes:recipe.html.twig',array(
+            'slug' => $slug
+        ));
+    }
+    
 }
