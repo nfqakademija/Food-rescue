bfd559700d0fa71e8c12deb1ca08cbb259c4b957
r0lakas <rolis.premongmail.com>
2014-04-29 18:02:38 +0300

    prod paemimas

--- src/FrameWorkersTM/FoodRescue/FoodAppBundle/Controller/RecipesController.php
+++ src/FrameWorkersTM/FoodRescue/FoodAppBundle/Controller/RecipesController.php
@@ -19,25 +19,128 @@ class RecipesController extends Controller
     {   
         $session = $request->getSession();
         $array['logged'] = $session->get('logged');
-/* irasymui
-        $recipe = new Recipes();
-        $recipe->setName('A Foo Bar');
-        $recipe->setPrice('19.99');
-        $recipe->setDescription('Lorem ipsum dolor');
 
         $em = $this->getDoctrine()->getManager();
-        $em->persist($recipe);
-        $em->flush();
-*/
-        $recipe = $this->getDoctrine()
-            ->getRepository('FrameWorkersTMFoodRescueFoodAppBundle:Recipes')
-            ->find('1');
-var_dump($recipe);
-        if (!$recipe) {
-            throw $this->createNotFoundException(
-                'No product found for id '.$id
-            );
+        $recipes = $em->getRepository('FrameWorkersTMFoodRescueFoodAppBundle:Recipes')
+            ->findRecipes(1);
+
+        var_dump($recipes);
+        foreach ($recipes as $recipe){
+            //echo $recipe->getName()."<br/>";
         }
+//get products from recipe
+        //$em = $this->getDoctrine()->getManager();
+        //$products = $em->getRepository('FrameWorkersTMFoodRescueFoodAppBundle:Recipes')
+        //    ->findAllProductsByRecipeId(1);
+        //var_dump($products);
+        //foreach ($products as $product){
+            //echo $product->getproductsId()->getName()."<br/>";
+        //}
+
+//get first 10 recipes names
+        //$em = $this->getDoctrine()->getManager();
+        //$products = $em->getRepository('FrameWorkersTMFoodRescueFoodAppBundle:Recipes')
+        //    ->getProducts();
+        //var_dump($products['0']->getName());
+        //var_dump($products['0']->getRecipes());
+
+
+
+
+       // foreach ($b as $a){
+            //var_dump($a);
+       // }
+        //foreach ($products as $product){
+           //echo $product['0']->getName()."<br/>";
+          // var_dump($product['0']->getRecipeProducts()); echo "<br/>";
+        //}
+
+        //$rep = $this->getDoctrine()
+        //    ->getRepository('FrameWorkersTMFoodRescueFoodAppBundle:Recipes');
+        //$product = $rep->findOneBy(
+        //    array('id' => '1'));
+        //echo "recipe by id:"; var_dump($product->getRecipes()); echo "<br/>";
+
+        //$rep = $this->getDoctrine()
+        //    ->getRepository('FrameWorkersTMFoodRescueFoodAppBundle:RecipesProducts');
+        //$product = $rep->findAll();
+
+
+
+        // query by the primary key (usually "id")
+        //$product = $repository->find($id);
+        //echo "recipe by id:"; var_dump($product); echo "<br/>";
+
+        // dynamic method names to find based on a column value
+        //$product = $repository->findOneById($id);
+        //echo "recipe by id:"; var_dump($product); echo "<br/>";
+
+        //rasti viena produkta pagal pavadinima
+       // $product = $repository->findOneByName('Blyneliai');
+       // var_dump($product);
+
+        // find *all* products
+       // $products = $repository->findAll();
+       // echo "all recipes:"; var_dump($products); echo "<br/>";
+
+        // find a group of products based on an arbitrary column value
+        //$products = $repository->findByName('Blyneliai');
+        //echo "recipes by name:"; var_dump($products); echo "<br/>";
+
+        // query for one product matching be name and price
+       // $product = $repository->findOneBy(
+        //    array('name' => 'Blyneliai', 'price' => 19.99)
+        //);
+
+        // query for all products matching the name, ordered by price
+        //$products = $repository->findBy(
+        //    array('name' => 'foo'),
+        //    array('price' => 'ASC')
+        //);
+
+
+        //query builder
+        // get all recipes DESC
+        /*
+        $repository = $this->getDoctrine()
+            ->getRepository('FrameWorkersTMFoodRescueFoodAppBundle:Recipes');
+
+        $query = $repository->createQueryBuilder('p')
+            ->where('p.name > :namee')
+            ->setParameter('namee', 'Blyneliai')
+            ->orderBy('p.name', 'DESC')
+            ->getQuery();
+
+        $products = $query->getResult();
+        */
+//var_dump($products);
+//getResult() get an array of results.
+//getSingleResult() get one result (which throws exception there is no result) so use this below
+//getOneOrNullResult()
+
+        //DQL
+        /*
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+            'SELECT p
+            FROM FrameWorkersTMFoodRescueFoodAppBundle:Recipes p
+            WHERE p.name = :namee
+            ORDER BY p.name ASC'
+        )->setParameter('namee', 'Blyneliai');
+
+        $products = $query->getResult();
+        */
+        //var_dump($products['0']);
+
+
+        //$product = $this->getDoctrine()
+         //   ->getRepository('FrameWorkersTMFoodRescueFoodAppBundle:Recipes')
+        //    ->find('1');
+        //var_dump($product);
+        //$categoryName = $product->getaa()->getName();
+        //$product->getCategory()->getProductsId();
+        //$products->getName();
+
 
 
         return $this->render('FrameWorkersTMFoodRescueFoodAppBundle:Recipes:index.html.twig', $array);
