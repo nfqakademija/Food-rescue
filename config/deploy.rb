c6594ed41c17c6528a7bde8d56d0a01d6cc333bc
Karolis <karolis@webvision.lt>
2014-05-15 17:51:40 +0300

    Deploy settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, 'Food-rescue'
-set :repo_url, 'https://github.com/nfqakademija/Food-rescue'
+set :repo_url, 'https://github.com/nfqakademija/Food-rescue.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
