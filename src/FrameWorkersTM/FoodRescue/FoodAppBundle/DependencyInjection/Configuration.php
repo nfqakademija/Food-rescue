6dab966fda5098a9c504811eaee3f03523057464
Karolis <karolis@webvision.lt>
2014-05-22 11:37:09 +0300

    Minor fixes

--- src/FrameWorkersTM/FoodRescue/FoodAppBundle/DependencyInjection/Configuration.php
+++ src/FrameWorkersTM/FoodRescue/FoodAppBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('frame_workers_tm_food_rescue_food_app');
+        $treeBuilder->root('frame_workers_tm_food_rescue_food_app');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
