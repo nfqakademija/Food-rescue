3232fa99a14ea4c957e9cb2cb56aeecd67c9cc6e
Povilas Mankus <pmankus@gmail.com>
2014-05-25 20:02:12 +0300

    my products lentelės paėmimas ajax

--- web/assets/js/MyProduct.js
+++ web/assets/js/MyProduct.js
@@ -25,6 +25,83 @@ $.ajax({
         })
     }
 });
+function refreshMyProducts() {
+    $.ajax({
+        url: appUrl + 'myproductstable/',
+        contentType: "HTML",
+        success: function(result){
+            $("#myProductsTable").html(result);
+            $(".edit-input").hide();
+            $(".edit-text").show();
+            setupEditableTable();
+        }
+    });
+}
+function setupEditableTable() {
+    $(".editable_tr").click(function() {
+        var ID=$(this).attr('id');
+        $("#pro_quantity_"+ID).hide();
+        $("#pro_end_date_"+ID).hide();
+        $("#pro_quantity_input_"+ID).show();
+        $("#pro_end_date_input_"+ID).show();
+    } ).change(function() {
+        var ID=$(this).attr('id');
+        var quantity=$("#pro_quantity_input_"+ID).val();
+        var endDate=$("#pro_end_date_input_"+ID).val();
+        var dataString = 'id='+ ID +'&quantity='+quantity+'&endDate='+endDate;
+
+        if (quantity.length > 0 && endDate.length > 0) {
+            $.ajax( {
+                type: "POST",
+                url: appUrl + 'myproductsedit/',
+                data: dataString,
+                cache: false,
+                success: function(_result)
+                {
+                    if(_result.length < 1) {
+                        $('#edit_table_errors').html('');
+                        $("#pro_quantity_"+ID).html(quantity);
+                        $("#pro_end_date_"+ID).html(endDate);
+                    } else {
+                        var result = jQuery.parseJSON(_result);
+                        var errorString = "";
+                        result.forEach(function(err) {
+                            errorString = errorString + " " + err;
+                        });
+                        $('#edit_table_errors').text(errorString);
+                    }
+                }
+            });
+        }
+    });
+    $('.ui-icon-trash').click(function() {
+        var ID = $(this).attr('id').split('_')[1];
+        var name= $('#pro_name_' + ID).text();
+        console.log(ID);
+        if (confirm('Ar tikrai norite ištrinti produktą ' + name)) {
+            $.ajax({
+                url: appUrl + 'myproductsdelete/',
+                type: 'POST',
+                data:'id=' + ID,
+                success: function(response) {
+                    if (response == 'deleted'){
+                        $('#'+ ID).slideUp();
+                    }
+                }
+            });
+        }
+    });
+    $(".edit-endDate").datepicker({
+        monthNames: ['Sausis', 'Vasaris', 'Kovas', 'Balandis', 'Gegužė', 'Birželis', 'Liepa', 'Rugpjūtis', 'Rugsėjis', 'Spalis', 'Lapkritis', 'Gruodis'],
+        dayNamesMin: ['Pr', 'An', 'Tr', 'Kt', 'Pn', 'Š', 'S'],
+        dateFormat: 'yy/mm/dd'
+    });
+};
+
+$(document).ready(function() {
+    refreshMyProducts();
+});
+
 
 $('#addProductForm_endDate').datepicker({
     monthNames: ['Sausis', 'Vasaris', 'Kovas', 'Balandis', 'Gegužė', 'Birželis', 'Liepa', 'Rugpjūtis', 'Rugsėjis', 'Spalis', 'Lapkritis', 'Gruodis'],
@@ -32,49 +109,10 @@ $('#addProductForm_endDate').datepicker({
     dateFormat: 'yy/mm/dd'
 });
 
-$(".edit-endDate").datepicker({
-    monthNames: ['Sausis', 'Vasaris', 'Kovas', 'Balandis', 'Gegužė', 'Birželis', 'Liepa', 'Rugpjūtis', 'Rugsėjis', 'Spalis', 'Lapkritis', 'Gruodis'],
-    dayNamesMin: ['Pr', 'An', 'Tr', 'Kt', 'Pn', 'Š', 'S'],
-    dateFormat: 'yy/mm/dd'
-});
 
 
-$(".editable_tr").click(function() {
-    var ID=$(this).attr('id');
-    $("#pro_quantity_"+ID).hide();
-    $("#pro_end_date_"+ID).hide();
-    $("#pro_quantity_input_"+ID).show();
-    $("#pro_end_date_input_"+ID).show();
-} ).change(function() {
-    var ID=$(this).attr('id');
-    var quantity=$("#pro_quantity_input_"+ID).val();
-    var endDate=$("#pro_end_date_input_"+ID).val();
-    var dataString = 'id='+ ID +'&quantity='+quantity+'&endDate='+endDate;
 
-    if (quantity.length > 0 && endDate.length > 0) {
-        $.ajax( {
-            type: "POST",
-            url: appUrl + 'myproductsedit/',
-            data: dataString,
-            cache: false,
-            success: function(_result)
-            {
-                if(_result.length < 1) {
-                    $('#edit_table_errors').html('');
-                    $("#pro_quantity_"+ID).html(quantity);
-                    $("#pro_end_date_"+ID).html(endDate);
-                } else {
-                    var result = jQuery.parseJSON(_result);
-                    var errorString = "";
-                    result.forEach(function(err) {
-                        errorString = errorString + " " + err;
-                    });
-                    $('#edit_table_errors').html(errorString);
-                }
-            }
-        });
-    }
-});
+
 
 // Edit input box click action
 $(".edit-input").mouseup(function()
@@ -89,27 +127,3 @@ $(document).mouseup(function()
     $(".edit-text").show();
 });
 
-$(document).ready(function()
-{
-    $(".edit-input").hide();
-    $(".edit-text").show();
-});
-
-//remove product
-$('.ui-icon-trash').click(function() {
-    var ID = $(this).attr('id').split('_')[1];
-    var name= $('#pro_name_' + ID).text();
-console.log(ID);
-    if (confirm('Ar tikrai norite ištrinti produktą ' + name)) {
-        $.ajax({
-            url: appUrl + 'myproductsdelete/',
-            type: 'POST',
-            data:'id=' + ID,
-            success: function(response) {
-                if (response == 'deleted'){
-                    $('#'+ ID).slideUp();
-                }
-            }
-        });
-    }
-});
